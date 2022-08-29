9dcbda16484c306af0f70b389897dc154adfd5e2
jungsun.yoo <alskd82@gmail.com>
2022-08-29 17:19:02 +0900

    퍼블리싱

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'src')
 
-const outDir = resolve(__dirname, 'dist')            /* 메인 빌드용 */  
-// const outDir = resolve(__dirname, 'dist/contentPage')   /* CMS 컨텐츠 페이지 빌드용 */
+const outDir = resolve(__dirname, 'dist');
 
 function CustomHmr() {
     return {
@@ -39,35 +38,19 @@ export default defineConfig({
 
         rollupOptions: {
             input: {
-
-                /* 메인 빌드용 */
                 index: resolve(root, "index.html"),
-                history: resolve(root, "01_history.html"),
-                impact: resolve(root, "02_impact.html"),
-                service: resolve(root, "03_service.html"), 
-                newsroom: resolve(root, "04_newsroom.html"), 
-                
-                fromournewsroom: resolve(root, "fromournewsroom/from-our-newsroom" ,"index.html"),
+                m_index: resolve(root, "m_index.html")
 
-                m_index: resolve(root, "m", "m.html"),
-                m_history: resolve(root, "m", "m_01_history.html"),
-                m_impact: resolve(root, "m", "m_02_impact.html"),
-                m_service: resolve(root, "m","m_03_service.html"), 
-                m_newsroom: resolve(root, "m","m_04_newsroom.html"), 
-
-
-                /* CMS 컨텐츠 페이지 빌드용 */
-                // contentPage: resolve(root, "contentPage", "contentPage.html"),  
             },
 
             /* out 을 명시 안하면 assets 폴더 안에 다 담김 */
             output: {
                 // entryFileNames: 'assets/[name]-[hash].js',   // works
                 // chunkFileNames: 'assets/[name]-[hash].js',   // works
-                assetFileNames: assetInfo => { /*  사용한 소스 중 js 파일 이외의 것들 처리 (css img 등) */
-                    if (/\.css$/.test(assetInfo.name)) return 'assets/css/[name]-[hash][extname]'
-                    return 'assets/img/[name]-[hash][extname]'
-                }
+                // assetFileNames: assetInfo => { /*  사용한 소스 중 js 파일 이외의 것들 처리 (css img 등) */
+                //     if (/\.css$/.test(assetInfo.name)) return 'assets/css/[name]-[hash][extname]'
+                //     return 'assets/img/[name]-[hash][extname]'
+                // }
             }
         },
 
